a5ec005e926159d446a8eab8f7aedec93b51adbe
Roland Fulde <rfuxx@users.noreply.github.com>
2019-01-27 17:58:17 +0100

    bump version (8)

--- app/build/generated/source/buildConfig/debug/de/westfalen/fuldix/aspectslider/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/de/westfalen/fuldix/aspectslider/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "de.westfalen.fuldix.aspectslider";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 7;
-  public static final String VERSION_NAME = "0.2.2";
+  public static final int VERSION_CODE = 8;
+  public static final String VERSION_NAME = "0.2.3";
 }
